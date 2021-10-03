8c706b63a88a4af537fd7e98f174fafed6f2bbe9
cat_or_not <proovigo48@gmail.com>
2021-10-03 18:43:56 -0400

    making this my bots repository

---
+++ ruler_game_bot/README.md
@@ -0,0 +1,3 @@
+# the_ruler_game_bot
+This is a bot that plays the ruler in inches. here is a demo of this : https://youtu.be/y3oMfjk1DV8 . 
+the website the bot uses : https://www.rulergame.net/new-english-ruler-game.php
---
+++ ruler_game_bot/main.py
@@ -0,0 +1,91 @@
+import pyautogui
+from PIL import Image
+#from pytesseract import *
+import win32api, win32con
+import win32clipboard
+import webbrowser
+import keyboard
+from time import sleep
+
+sleep(2)
+print("Ready")
+sleep(0.5)
+print("waiting for next location of the copy place")
+# webbrowser.open("https://www.rulergame.net/new-english-ruler-game.php", new=2)
+#https://www.rulergame.net/new-english-ruler-game.php
+#Note: You have to change the path below to your tesseract.exe location
+
+
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,0,0)
+    sleep(0.05) #This pauses the script for 0.1 seconds
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,0,0)
+
+def getclip():
+    win32clipboard.OpenClipboard()
+    data = win32clipboard.GetClipboardData()
+    win32clipboard.CloseClipboard()
+    return data
+
+def get_inch(xy):
+    click(xy[0],xy[1])
+    click(xy[0],xy[1])
+    click(xy[0],xy[1])
+    pyautogui.keyDown('ctrl')
+    pyautogui.press('c')
+    pyautogui.keyUp('ctrl')
+
+    return getclip().replace('"','')
+
+
+#def img_text(x1,y1,x2,y2):
+#    pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+#
+#    img = pyautogui.screenshot("bruh.png",region=(x1,y1,x2-x1,y2-y1))
+#
+#    return pytesseract.image_to_string(img)
+
+location_of_text = [(None,None),(None,None)]
+x = 0
+while keyboard.is_pressed('q') == False:
+    if win32api.GetKeyState(0x01) < 0:
+        location_of_text[x] = tuple(pyautogui.position())
+        print(f"location {location_of_text[x]}")
+        x += 1
+        sleep(0.5)
+        print("waiting for next location of the field")
+        if x == 1:
+            break
+
+location = [(None,None),(None,None)]
+x = 0
+while keyboard.is_pressed('q') == False:
+    if win32api.GetKeyState(0x01) < 0:
+        location[x] = tuple(pyautogui.position())
+        print(f"location {location[x]}")
+        x += 1
+        sleep(0.5)
+        print("waiting for next location of the field")
+        if x == 2:
+            break
+
+print("starting in 1 second")
+sleep(1)
+print("started")
+
+y = (location[1][1] - location[0][1]) * 0.4 + location[0][1]
+
+while keyboard.is_pressed('q') == False:
+    text = get_inch(location_of_text[0])
+    # print(eval(f"{location[1][0]-location[0][0]} * ({text})"))
+
+    place = int(eval(f"{location[1][0]-location[0][0]} * ({text})") + location[0][0])
+    click(place,int(y))
+    sleep(0.001)
+    
+
+
+
+
